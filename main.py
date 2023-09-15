dbd09614ca320fba0bb351793c56cfcbe641e841
pibolib <pibolib@gmail.com>
2023-09-14 21:26:14 -0400

    xp system functional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import datetime
 from random import randint
 from discord import app_commands
 from dotenv import load_dotenv
@@ -17,6 +18,8 @@ class User:
     def __init__(self, id: int) -> None:
         self.__id = id
         self.__xp = 0
+        self.__last_message = datetime.datetime(1900, 1, 1)
+        self.__level = 0
 
     def get_id(self) -> int:
         return self.__id
@@ -26,6 +29,33 @@ class User:
 
     def get_xp(self) -> int:
         return self.__xp
+
+    def get_last_message_timestamp(self) -> datetime.datetime:
+        return self.__last_message
+
+    def update_last_message_timestamp(self) -> None:
+        self.__last_message = datetime.datetime.now()
+
+    def get_level(self) -> str:
+        return self.__level
+
+    def set_level(self, level: int) -> None:
+        self.__level = level
+
+class Level:
+    def __init__(self, name: str, threshold: int, id: int = -1) -> None:
+        self.__name = name
+        self.__threshold = threshold
+        self.__id = id
+
+    def get_name(self) -> str:
+        return self.__name
+
+    def get_threshold(self) -> int:
+        return self.__threshold
+
+    def get_id(self) -> int:
+        return self.__id
 
 def db_has_user(user_id: int):
     for user in user_db:
@@ -47,9 +77,13 @@ guild: int = discord.Object(os.getenv('GUILD_ID'))
 intents = discord.Intents.default()
 intents.message_content = True
 
-user_db: list = []
-
 client = BotClient(intents=intents)
+
+# initialize all actual variables related to bot functions
+
+user_db = []
+
+levels = [Level("Beginner", 0), Level("Apprentice", 5, 1057176952559124560), Level("Journeyman", 15, 1057176994883833966), Level("Adept", 40, 1057177012135022695), Level("Master", 75, 1057177029000306738), Level("Grandmaster", 125, 1085465765316141147)]
 
 @client.event
 async def on_ready():
@@ -65,20 +99,64 @@ async def on_message(message):
     if not db_has_user(author_id):
         user_db.append(User(author_id))
 
-    db_get_user(author_id).add_xp(1)
-
-    await message.channel.send(f'{message.author.mention} now has {db_get_user(author_id).get_xp()} xp!')
+    if (datetime.datetime.now() - db_get_user(author_id).get_last_message_timestamp()).total_seconds() >= 72000:
+        user_db_entry = db_get_user(author_id)
+        user_db_entry.update_last_message_timestamp()
+        user_db_entry.add_xp(1)
+        for i, level in enumerate(levels):
+            if level.get_threshold() == db_get_user(author_id).get_xp():
+                previous_level = levels[i-1].get_name()
+                current_level = levels[i].get_name()
+                # await message.author.remove_roles(level_roles)
+                # await message.author.add_role(level_roles[i])
+                user_db_entry.set_level(i)
+                await message.channel.send(f'{message.author.mention} ranks up! {previous_level} ⇒ {current_level}')
+                break
 
 # Implements /hello command. Syntax: /hello
 @client.tree.command(description = "Say hello to liliumbot!")
 async def greet(interaction: discord.Interaction):
     await interaction.response.send_message("Hello, I'm liliumbot!")
 
-# Implements /roll command. Syntax: /roll <max-1: int>
+# Implements /roll command. Syntax: /roll <max: int>
 @client.tree.command(description = "Roll a random number between 0 and max (default 6).")
 async def roll(interaction: discord.Interaction, max: int = 6):
     await interaction.response.send_message(f'You rolled a {randint(1, max)}!')
 
-# Implements /xp command. Syntax: /xp 
+# Implements /addxp command. Syntax: /setxp <user: discord.User> <amount: int>
+@client.tree.command(description = "Adds XP to user. Levels user up if they reach any thresholds.")
+async def addxp(interaction: discord.Interaction, user: discord.User, xp: int):
+    if not interaction.user.guild_permissions.administrator:
+        await interaction.response.send_message("You do not have the required permissions to use this command.", ephemeral=True)
+        return
+    if not db_has_user(user.id):
+        user_db.append(User(user.id))
+
+    user_db_entry = db_get_user(user.id)
+    response = f'Gave {xp} XP to {user.mention}.'
+    for n in range(xp):
+        user_db_entry.add_xp(1)
+        for i, level in enumerate(levels):
+            if level.get_threshold() == user_db_entry.get_xp():
+                previous_level = levels[i-1].get_name()
+                current_level = levels[i].get_name()
+                response += f'\n{user.mention} ranks up! {previous_level} ⇒ {current_level}'
+                user_db_entry.set_level(i)
+                break
+    await interaction.response.send_message(response)
+
+# Implements /xp command. Syntax: /setxp <user: discord.User>
+@client.tree.command(description = "Lists statistics about a given user's xp status.")
+async def xp(interaction: discord.Interaction, user: discord.User = None):
+    u = None
+    if user is None:
+        u = interaction.user
+    else:
+        u = user
+    if not db_has_user(u.id):
+        user_db.append(User(u.id))
+    user_db_entry = db_get_user(u.id)
+    response = f'{u.mention}\'s XP status: ```Rank: {levels[user_db_entry.get_level()].get_name()}\nXP: {user_db_entry.get_xp()}```'
+    await interaction.response.send_message(response)
 
 client.run(token)
